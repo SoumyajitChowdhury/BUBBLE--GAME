4a90f19084b4c47a7f4e34ddb7a1a64cf20bbb63
Soumyajit Chowdhury <116186474+SoumyajitChowdhury@users.noreply.github.com>
2023-08-12 19:02:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+let timer = 60;
+let score = 0;
+let hitrn = 0; 
+function inctraseScore(){
+    score += 10;
+    document.querySelector("#scoreVal").textContent = score;
+}
+function runTimer(){
+    let timerint = setInterval(function(){
+        if(timer>0){
+            timer--;
+            document.querySelector("#timerVal").textContent = timer;
+        }
+        else{
+            clearInterval(timerint);
+            document.querySelector("#pbtn").innerHTML = `<h1 id = "gameOver">GAME OVER </h1>`;
+            document.querySelector("#gameOver").style.color = 'red';
+            document.querySelector("#gameOver").style.fontSize = '100px';
+        }
+        
+    },1000);
+}
+
+function makeBubble(){
+    let clutter = "";
+
+    for(let i = 1;i<=133;i++){
+        let rn = Math.floor(Math.random()*10);
+        clutter += `<div class="bubble">${rn}</div>`;
+    }
+
+    document.querySelector("#pbtn").innerHTML = clutter;
+}
+
+function getNewHit(){
+    hitrn = Math.floor(Math.random()*10);
+    document.querySelector("#hitVal").textContent = hitrn;
+}
+
+
+document.querySelector("#pbtn").addEventListener("click", function(details){
+    let clickedNnumber = Number(details.target.textContent);
+    if(clickedNnumber === hitrn){
+        inctraseScore();
+        makeBubble();
+        getNewHit();
+    }
+})
+
+
+runTimer();
+makeBubble();
+getNewHit();
+// inctraseScore();
